b1467a534353490708251de376b1e76cf8b5c50d
MrBarnesTVDSB <90469601+MrBarnesSDCI@users.noreply.github.com>
2021-10-19 13:45:40 -0400

    Add files via upload

---
+++ student.hpp
@@ -0,0 +1,16 @@
+#pragma once // prevents multiple definitions
+
+#include <iostream> // print library
+#include <string>
+using namespace std;
+
+// ===============
+// implementations
+
+//Replace string function
+bool divisableByFive(int a, int b)
+{
+	return 0; // TODO: fix
+}
+
+
